72a15674419d0dca108c01566cfbb732e0b8e8ca
JustinasKiznis <44317919+JustinasKiznis@users.noreply.github.com>
2021-01-28 10:59:13 +0200

    Prepare stage

---
+++ src/test/java/utils/ComponentUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+public class ComponentUtils {
+}
