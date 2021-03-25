94a7f375f9f586d91f44a61ba6891182bd80f502
giebar <giedre1996@gmail.com>
2021-03-25 09:33:38 +0200

    Add cross browser testing

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,18 +3,42 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     public WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeClass
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    @Parameters("browser")
+    public void setup(String browser) throws Exception {
+        if(browser.equalsIgnoreCase("Firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        else if(browser.equalsIgnoreCase("Chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else if(browser.equalsIgnoreCase("Edge")){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        else{
+            //If no browser passed throw exception
+            throw new Exception("Browser is not correct");
+        }
         driver.manage().window().maximize();
         driver.get("https://festodeskmanagementqa.azurewebsites.net/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterClass
@@ -22,3 +46,6 @@ public class BaseTest {
         driver.quit();
     }
 }
+
+
+
